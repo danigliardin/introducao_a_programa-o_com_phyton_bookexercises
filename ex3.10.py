fbcd7bc5900ce9199067ccab066dd992c6f76c0c
Danilo Gliardin <97619501+danigliardin@users.noreply.github.com>
2022-09-07 20:07:40 -0300

    Delete ex3.10.py

--- ex3.10.py
+++
@@ -1,7 +0,0 @@
-salario = float(input("Salario: "))
-porcentagem = float(input("Porcentagem do Aumento: "))
-
-aumento = porcentagem * 10
-aumento_real = salario + aumento
-print(aumento)
-print(aumento_real)
