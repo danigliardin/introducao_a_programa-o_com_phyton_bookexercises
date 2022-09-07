94fd7bbc1426f8ce15d02968392400fc19e37e6c
Danilo Gliardin <97619501+danigliardin@users.noreply.github.com>
2022-09-07 20:08:19 -0300

    Delete ex3.12.py

--- ex3.12.py
+++
@@ -1,6 +0,0 @@
-lugar_de_destino = input("Local de Destino: ")
-distancia = float(input("Distancia a Percorrer em Kilometros: "))
-velocidade = float(input("Velocidade Média Esperada em KM/H: "))
-
-velocidade_media = distancia/velocidade
-print(velocidade_media)
