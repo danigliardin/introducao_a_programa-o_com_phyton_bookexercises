704516cc3b89d2fe1b93a7296dedb43dbafa6199
Danilo Gliardin <97619501+danigliardin@users.noreply.github.com>
2022-09-07 20:07:50 -0300

    Delete ex3.11.py

--- ex3.11.py
+++
@@ -1,8 +0,0 @@
-mercadoria = input("Mercadoria: ")
-preco_mercadoria = float(input("Preço Mercadoria: "))
-desconto_mercadoria = float(input("Desconto em Decimal: "))
-
-desconto = preco_mercadoria * desconto_mercadoria
-print(mercadoria)
-print(preco_mercadoria)
-print(desconto)
