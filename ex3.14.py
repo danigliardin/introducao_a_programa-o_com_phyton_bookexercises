901d53f56c6c5783905f312629e02bf362bf650b
Danilo Gliardin <97619501+danigliardin@users.noreply.github.com>
2022-09-07 20:08:36 -0300

    Delete ex3.14.py

--- ex3.14.py
+++
@@ -1,5 +0,0 @@
-km_rodados = float(input("Quantidade de Kilometros percorridos: "))
-dias = float(input("Quantidade de dias com o Veiculo: "))
-
-preco = (km_rodados * 0.15) + (dias * 60)
-print(preco)
